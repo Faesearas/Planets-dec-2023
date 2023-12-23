ab65c8d8a19f42accd21dbb0ca7d4d3790a15a20
AmyHruska <amyhruskacda@gmail.com>
2023-12-23 14:56:51 -0800

    added martian age test

--- __tests__/planetMath.test.js
+++ __tests__/planetMath.test.js
@@ -6,7 +6,7 @@ describe('planetMath', () => {
   let planetObj;
 
   beforeEach(() => {
-    userDate = new PlanetMath("1981-01-01");
+    userDate = new PlanetMath("1981-12-20");
     planetObj = new PlanetObj();
   });
 
@@ -14,6 +14,9 @@ describe('planetMath', () => {
     userDate.yearCompare();
     expect(userDate.userAge).toEqual(42);    
   });
-
+  test('should display the difference between the entered date and now in martian orbital periods', () => {    
+    userDate.yearCompare();
+    expect(userDate.marAge).toEqual(unknown);    
+  });
 
 });
