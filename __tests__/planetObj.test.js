ee64a051b9ca17634b253726fbf321223c94b36a
AmyHruska <amyhruskacda@gmail.com>
2023-12-23 13:47:01 -0800

    add new red planetObj test for Mercury

--- __tests__/planetObj.test.js
+++ __tests__/planetObj.test.js
@@ -2,16 +2,21 @@ import PlanetObj from '../src/js/planetObj.js';
 
 describe('planetMath', () => {
     let planetObj;
-  
+
     beforeEach(() => {
-      planetObj = new PlanetObj();
+        planetObj = new PlanetObj();
     });
 
-    test('should display a earth orbital period worth of milliseconds', () => {
+    test('should display an earth orbital period worth of milliseconds', () => {
         expect(planetObj.earth).toEqual(31558118400);
-      });
-      test('should display a earth orbital period worth of milliseconds', () => {
+    });
+
+    test('should display an earth day worth of milliseconds', () => {
         expect(planetObj.ertDay).toEqual(86400000);
-      });
+    });
+
+    test('should display the amount of Mercury orbital periods to equal 1 earth orbital period', () => {
+        expect(planetObj.mer).toEqual(4.15);
+    });
 
 });
