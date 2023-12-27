04d6acf2d4997a2c57fefb5936e3070eb310d2ce
AmyHruska <amyhruskacda@gmail.com>
2023-12-27 08:52:32 -0800

    add test for time unil next birthday on mars

--- __tests__/planetMath.test.js
+++ __tests__/planetMath.test.js
@@ -6,16 +6,16 @@ describe('planetMath', () => {
   let planetObj;
 
   beforeEach(() => {
-    userDate = new PlanetMath("1981-12-20");
+    userDate = new PlanetMath("1981-12-20"); 
     planetObj = new PlanetObj();
   });
 
   test('should display the difference between the entered date and now', () => {    
     userDate.yearCompare(0);
-    expect(userDate.userAge).toEqual(42);    
+    expect(userDate.userAge).toEqual(42); 
   });
 
-  test('should display the difference between the entered date and now, in martian orbital periods!', () => {    
+  test('should display the user`s entered age, in martian orbital periods!', () => {    
     userDate.yearCompare(3);
     expect(userDate.pntAge).toEqual("22.34");
   });
