96a2e006c5dac9c61c5351e68eb6ea24ebc00000
AmyHruska <amyhruskacda@gmail.com>
2024-02-14 16:25:03 -0800

    added basic UI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import planetMath from './js/planetMath.js';
+import PlanetMath from './js/planetMath.js';
+
+function planetCalc(e) {
+  e.preventDefault();
+  let planetInput = document.getElementById("inputPlanet").value;
+  let userDateInput = document.getElementById("inputDate").value;
+  let userDate = new PlanetMath(userDateInput);
+  userDate.yearCompare(planetInput);
+  document.getElementById("earthResult").innerText = userDate.userAge;
+  document.getElementById("selectionResult").innerText = userDate.pntAge;
+}
 
 window.addEventListener("load", function () {
-  
+  document.querySelector("form#planetCalc").addEventListener("submit", planetCalc);
 });
